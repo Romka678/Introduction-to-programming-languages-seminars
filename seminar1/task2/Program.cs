84b8dfd0d6f98275f4109f9310b32e3d1617e2b8
Роман Вятчанин <viat4anin@gmail.com>
2022-09-01 19:15:25 +0300

    Task from seminar and Homewrok №1

---
+++ seminar1/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a==b*b)
+{
+Console.WriteLine("Да");
+}
+else{
+Console.WriteLine("Нет");
+}
---
+++ seminar1/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//	456 -> 6
+//	782 -> 2
+//	918 -> 8
+Console.WriteLine("Введите трехзначное число:");
+int N = Convert.ToInt32(Console.ReadLine());
+while((N > 999 || N < -999) || (N < 99 && N >= 0) || (N > -99 && N <= 0))
+{
+    Console.WriteLine("Число не трехзначное.");
+    Console.WriteLine("Введите трехзначное число:");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+int res = N%10;
+Console.WriteLine(Math.Abs(res));
