e2e0b9419fd52f9bdf24ebd3083400d2cd1667d0
Роман Вятчанин <viat4anin@gmail.com>
2022-09-01 19:43:15 +0300

    Done Homework

---
+++ HomeWork_from_seminar1/HW_task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt16(Console.ReadLine());
+if(a > b){
+    Console.WriteLine("max =" + a);
+    Console.WriteLine("min =" + b);
+}
+else{
+    Console.WriteLine("max =" + b);
+    Console.WriteLine("min =" + a);
+}
---
+++ HomeWork_from_seminar1/HW_task8/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt16(Console.ReadLine());
+int i = 1;
+if(N == 1) Console.WriteLine("Четных чисел нет.");
+else{
+    if(N > 1){
+        Console.Write("Все четные числа от 1 до " + N + ":");
+        while(i <= N){
+            if(i%2 == 0){
+                Console.Write(" " + i);
+                i++;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    else{
+        Console.Write("Все четные числа от 1 до " + N + ":");
+        while(i >= N){
+        if(i%2 == 0){
+            Console.Write(" " + i);
+            i--;
+        }
+        else{
+            i--;
+        }
+    }
+    }
+}
+
+
