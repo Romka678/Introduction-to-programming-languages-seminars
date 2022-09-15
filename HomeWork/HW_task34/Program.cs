811b22b5f102b399f69a74dff6b7c3e511a24092
Роман Вятчанин <viat4anin@gmail.com>
2022-09-15 19:42:40 +0300

    Done HW from seminar5

---
+++ HomeWork/HW_task34/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] SetArray()
+{
+    int[] arr = new int[10];
+    Random rand = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(100,1000);
+    }
+    return arr;
+}
+
+void ShowArray(int[] arr)
+{
+    string str = "";
+    for(int i = 0; i < arr.Length; i++)
+    {
+        str = str + arr[i];
+    }
+    Console.WriteLine(string.Join("," , arr));
+}
+
+int GetNumber(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]%2 == 0) count++;
+    }
+    return count;
+}
+
+int[] arr = SetArray();
+int count = GetNumber(arr);
+ShowArray(arr);
+Console.WriteLine($"Количество четных элементов массива : {count}");
---
+++ HomeWork/HW_task36/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] SetArray()
+{
+    int[] arr = new int[10];
+    Random rand = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(1,11);
+    }
+    return arr;
+}
+
+void ShowArray(int[] arr)
+{
+    string str = "";
+    for(int i = 0; i < arr.Length; i++)
+    {
+        str = str + arr[i];
+    }
+    Console.WriteLine(string.Join("," , arr));
+}
+
+int SummNumber(int[] arr)
+{
+    int sum = 0;
+    for(int i = 1; i < arr.Length; i+=2)
+    {
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int[] arr = SetArray();
+int sum = SummNumber(arr);
+ShowArray(arr);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях : {sum}");
