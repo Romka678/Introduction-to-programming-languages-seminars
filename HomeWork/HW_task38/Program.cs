d38148d0f08754dac118f26bab2b6561217dae38
Роман Вятчанин <viat4anin@gmail.com>
2022-09-15 19:41:40 +0300

    Done HW from seminar5

---
+++ HomeWork/HW_task38/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] SetArray()
+{
+    int[] arr = new int[10];
+    Random rand = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(1,100);
+    }
+    return arr;
+}
+
+void ShowArray(int[] arr)
+{
+    string str = "";
+    for(int i = 0; i < arr.Length; i++)
+    {
+        str = str + arr[i];
+    }
+    Console.WriteLine(string.Join("," , arr));
+}
+
+int ResultOfMaxAndMin(int[] arr)
+{
+    int res = 0;
+    int min = arr[0];
+    int max = arr[0];
+    for(int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i] > max) max = arr[i];
+        else if(arr[i] < min) min = arr[i];
+        
+    }
+    res = max - min;
+    return res;
+}
+
+int[] arr = SetArray();
+int res = ResultOfMaxAndMin(arr);
+ShowArray(arr);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива : {res}");
